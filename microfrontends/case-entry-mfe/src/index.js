49a7c5eaa510e0b13f97d21e41f9cb040b1ccc0d
unknown <raxkaynan@gmail.com>
2023-01-18 23:40:02 +0800

    CDPPOC-4 Add case-entry-mfe scaffolding
    Add keycloak integration, api claim, style and build configs

---
+++ microfrontends/case-entry-mfe/src/index.js
@@ -0,0 +1,72 @@
+import './utils/public-path';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import styles from './index.css';
+import App from './App';
+import { KeycloakProvider } from './auth/Keycloak';
+
+class CaseEntryMFE extends HTMLElement {
+  #rootID = 'case-entry-mfe'
+  #appInstance = null
+
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: 'open' });
+  }
+
+  static get observedAttributes() {
+    return ['config'];
+  }
+
+  connectedCallback() {
+    this.render()
+  }
+
+  attributeChangedCallback(_, oldValue, newValue) {
+    if (newValue !== oldValue) {
+      this.render();
+    }
+  }
+
+  cleanTree() {
+    const currentElement = this.shadowRoot.getElementById(this.#rootID);
+
+    if (currentElement) {
+      this.shadowRoot.removeChild(currentElement);
+    }
+
+    this.#appInstance?.unmount();
+  }
+
+  render() {
+    const attrConf = this.getAttribute('config');
+    const config = attrConf && JSON.parse(attrConf);
+
+    if (config) {
+      const shadowRootElement = document.createElement('div');
+
+      shadowRootElement.id = this.#rootID;
+
+      this.cleanTree();
+
+      styles.use({ target: this.shadowRoot });
+
+      this.#appInstance = ReactDOM.createRoot(shadowRootElement);
+
+      this.#appInstance.render(
+        <React.StrictMode>
+          <KeycloakProvider>
+            <App config={config} />
+          </KeycloakProvider>
+        </React.StrictMode>
+      );
+
+      this.shadowRoot.appendChild(shadowRootElement);
+    }
+  }
+}
+
+customElements.define('case-entry-mfe', CaseEntryMFE);
