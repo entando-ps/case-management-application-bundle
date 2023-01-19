dba2f245e44d6b8fb0df2c6092fd806d024207bf
Matteo Emanuele Minnai <m.minnai@entando.com>
2023-01-19 15:51:22 +0100

    CDPPOC-8: cosmetic change

--- microservices/case-service/src/main/java/org/entando/bundle/controller/CaseController.java
+++ microservices/case-service/src/main/java/org/entando/bundle/controller/CaseController.java
@@ -14,12 +14,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Optional;
 
-@org.springframework.web.bind.annotation.RestController
+@RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CaseController {
