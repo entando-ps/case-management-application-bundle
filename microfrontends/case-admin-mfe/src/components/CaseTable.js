1bc55a42c27f5bdcfbebf327492bd4d9e5070c84
Rax Canaan W. Layumas <raxkaynan@gmail.com>
2023-02-07 17:07:28 +0800

    Implement admin case table sorting

--- microfrontends/case-admin-mfe/src/components/CaseTable.js
+++ microfrontends/case-admin-mfe/src/components/CaseTable.js
@@ -1,47 +1,110 @@
-import { Table } from 'react-bootstrap';
+import { useMemo } from 'react';
+import { Stack, Table } from 'react-bootstrap';
+import { useTable, useSortBy } from 'react-table';
 import { ReactComponent as ViewIcon } from 'bootstrap-icons/icons/eye-fill.svg';
 import { ReactComponent as DeleteIcon } from 'bootstrap-icons/icons/trash-fill.svg';
+import { ReactComponent as CaretUpIcon } from 'bootstrap-icons/icons/caret-up-fill.svg';
+import { ReactComponent as CaretDownIcon } from 'bootstrap-icons/icons/caret-down-fill.svg';
 import { Link } from 'react-router-dom';
 
 import deriveDisplayTextFromCaseState from '../utils/deriveDisplayTextFromCaseState';
 import formatDateToIt from '../utils/formatDateToIt';
 
-
 function CaseTable({ cases, onDeleteClick }) {
+  const columns = useMemo(() => [{
+    Header: 'Numero Domanda',
+    accessor: 'identifier',
+  }, {
+    Header: 'Codice Fiscale',
+    accessor: 'metadata.subscriber.fiscalCode',
+  }, {
+    Header: 'Regione',
+    accessor: 'metadata.subscriber.birthRegion',
+  }, {
+    Header: 'Settore',
+    accessor: 'metadata.subscriber.sector',
+  }, {
+    Header: 'Data Inserimento',
+    accessor: row => formatDateToIt(row.created),
+  }, {
+    Header: 'Stato',
+    accessor: row => deriveDisplayTextFromCaseState(row),
+  }, {
+    Header: 'Azioni',
+    id: 'id',
+    accessor: row => row,
+    disableSortBy: true,
+    Cell: ({ value: c }) => (
+      <div className="text-end">
+        <Link to={`/case/${c.id}`} style={{ color: 'inherit' }}>
+          <ViewIcon />
+        </Link>
+        <DeleteIcon
+          onClick={() => onDeleteClick(c)}
+          className="ms-4 me-2"
+          style={{ cursor: 'pointer' }}
+        />
+      </div>
+    )
+  }], [onDeleteClick]);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns,
+    data: cases,
+    disableSortRemove: true,
+  }, useSortBy);
+
   return (
-    <Table striped bordered hover>
+    <Table striped bordered hover {...getTableProps()}>
       <thead>
-        <tr>
-          <th>Numero Domanda</th>
-          <th>Codice Fiscale</th>
-          <th>Regione</th>
-          <th>Settore</th>
-          <th>Data Inserimento</th>
-          <th>Stato</th>
-          <th>Azioni</th>
-        </tr>
-      </thead>
-      <tbody>
-        {cases.map((c) => (
-          <tr key={c.id}>
-            <td>{c.identifier}</td>
-            <td>{c.metadata.subscriber.fiscalCode}</td>
-            <td>{c.metadata.subscriber.birthRegion}</td>
-            <td>{c.metadata.subscriber.sector}</td>
-            <td>{formatDateToIt(c.created)}</td>
-            <td>{deriveDisplayTextFromCaseState(c)}</td>
-            <td className="text-end">
-              <Link to={`/case/${c.id}`} style={{ color: 'inherit' }}>
-                <ViewIcon />
-              </Link>
-              <DeleteIcon
-                onClick={() => onDeleteClick(c)}
-                className="ms-4 me-2"
-                style={{ cursor: 'pointer' }}
-              />
-            </td>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th
+                {...column.getHeaderProps(column.getSortByToggleProps())}
+              >
+                <Stack direction="horizontal" gap={2} style={{ height: 24 }}>
+                  {column.render('Header')}
+                  {column.isSorted
+                    ? column.isSortedDesc
+                      ? <CaretDownIcon width={12} height={12} />
+                      : <CaretUpIcon width={12} height={12} />
+                    : column.canSort && (
+                      <Stack style={{ color: '#ADB5BD' }}>
+                        <CaretUpIcon width={12} height={12} />
+                        <CaretDownIcon width={12} height={12} />
+                      </Stack>
+                    )
+                  }
+                </Stack>
+              </th>
+            ))}
           </tr>
-        ))}
+         ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map(row => {
+           prepareRow(row)
+           return (
+             <tr {...row.getRowProps()}>
+               {row.cells.map(cell => {
+                 return (
+                   <td
+                     {...cell.getCellProps()}
+                   >
+                     {cell.render('Cell')}
+                   </td>
+                 )
+               })}
+             </tr>
+           )
+         })}
       </tbody>
     </Table>
   )
